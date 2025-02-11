a2888482176b23fefd9d287fc3e68196287e1b9c
Quamin Hua <quaminhua56@gmail.com>
2025-02-10 20:56:25 -0800

    Add reset button and prompt user for number
    Includes error handling, but grid does not change size yet.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const container = document.querySelector(".container");
 
 const MAX_GRID_SIZE = 960; // in pixels
 let gridSquares = 16; // number of squares on one side
-
 let squareSize = Math.floor(MAX_GRID_SIZE / gridSquares);
 
 const square = document.createElement('div');
@@ -19,3 +18,21 @@ for (let i = 0; i < gridSquares; ++i) {
 for (let i = 0; i < gridSquares; ++i) {
     container.appendChild(row.cloneNode(true));
 }
+
+const output = document.querySelector(".output");
+
+const button = document.querySelector(".resetBtn");
+button.addEventListener("click", function (event) {
+    let size = Number(prompt("Enter grid length (1-100):"));
+
+    if (!Number.isInteger(size)) {
+        output.textContent = "ERROR: Input was not an integer"
+    }
+    else if (size <= 0 || size > 100) {
+        output.textContent = "ERROR: Input was not in the range 1-100"
+    }
+    else {
+        output.textContent = "";
+        gridSquares = size;
+    }
+});
