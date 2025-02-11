5b473d8250e6f4783907c20e27c4b2a7e3c516e8
Quamin Hua <quaminhua56@gmail.com>
2025-02-10 21:41:26 -0800

    Implement reset button functionality
    The grid will reset and refill itself with the new value the user sets.

--- script.js
+++ script.js
@@ -2,25 +2,29 @@ const container = document.querySelector(".container");
 
 const MAX_GRID_SIZE = 960; // in pixels
 let gridSquares = 16; // number of squares on one side
-let squareSize = Math.floor(MAX_GRID_SIZE / gridSquares);
 
 const square = document.createElement('div');
 square.classList.add("square");
-square.style.cssText = `width: ${squareSize}px;
-                        height: ${squareSize}px;`;
 
 const row = document.createElement('div');
 row.style.display = "flex";
 
-for (let i = 0; i < gridSquares; ++i) {
-    row.appendChild(square.cloneNode());
-}
-for (let i = 0; i < gridSquares; ++i) {
-    container.appendChild(row.cloneNode(true));
+function resetGrid() {
+    container.textContent = "";
+    row.textContent = "";
+    const squareSize = Math.floor(MAX_GRID_SIZE / gridSquares);
+    square.style.width = `${squareSize}px`
+    square.style.height = `${squareSize}px`;
+
+    for (let i = 0; i < gridSquares; ++i) {
+        row.appendChild(square.cloneNode());
+    }
+    for (let i = 0; i < gridSquares; ++i) {
+        container.appendChild(row.cloneNode(true));
+    }
 }
 
 const output = document.querySelector(".output");
-
 const button = document.querySelector(".resetBtn");
 button.addEventListener("click", function (event) {
     let size = Number(prompt("Enter grid length (1-100):"));
@@ -34,5 +38,8 @@ button.addEventListener("click", function (event) {
     else {
         output.textContent = "";
         gridSquares = size;
+        resetGrid();
     }
 });
+
+resetGrid();
